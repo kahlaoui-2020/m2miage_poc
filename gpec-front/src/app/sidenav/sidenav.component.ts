6fbf47cb5e598721c72a40a16d1de0d16514e151
kahlaoui-2020 <kahlaoug@gmail.com>
2021-12-21 21:31:16 +0100

    add sidenav component

---
+++ gpec-front/src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import {UserService} from "../services/user.service";
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.css']
+})
+export class SidenavComponent implements  OnInit{
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver,private userService: UserService) {
+
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
