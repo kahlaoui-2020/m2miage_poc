dfaa26e6326f3a2aa2cba65a01a58f51a88a22ab
kahlaoui-2020 <kahlaoug@gmail.com>
2021-12-21 21:18:47 +0100

    add rout & MatModule

--- gpec-front/src/app/app-routing.module.ts
+++ gpec-front/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {RouteGuard} from "./services/route-guard";
+import {AppComponent} from "./app.component";
+import {SidenavComponent} from "./sidenav/sidenav.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent },
+  {path: 'home', component: SidenavComponent,
+    children: [
+      {path: '', component: DashboardComponent}
+    ] ,canActivate:[RouteGuard]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- gpec-front/src/app/app.module.ts
+++ gpec-front/src/app/app.module.ts
@@ -3,14 +3,51 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import {MatCardModule} from "@angular/material/card";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import {MatTabsModule} from "@angular/material/tabs";
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import {HttpClientModule} from "@angular/common/http";
+import { EditSkillsComponent } from './edit-skills/edit-skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    SidenavComponent,
+    UserProfileComponent,
+    EditSkillsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    HttpClientModule,
+
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
