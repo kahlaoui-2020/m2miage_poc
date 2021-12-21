6f3fed23bbf0ba1da829a8006f103acc7e86f988
kahlaoui-2020 <kahlaoug@gmail.com>
2021-12-21 21:44:09 +0100

    add user component

---
+++ gpec-front/src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import {UserService} from "../services/user.service";
+import {DomSanitizer, SafeUrl} from "@angular/platform-browser";
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css']
+})
+export class UserProfileComponent implements OnInit {
+
+  user!: User;
+  imageurl!: SafeUrl;
+
+  constructor(private userService: UserService, private domSanitizer: DomSanitizer) {
+
+  }
+
+  ngOnInit(): void {
+    this.userService.user.subscribe((user) => {
+      this.user = user
+      console.log('user', this.user)
+      console.log('photo', user.url_photo)
+      this.base64ToImage(user.url_photo.data)
+
+    })
+
+
+  }
+
+  base64ToImage(data: any) {
+    let TYPED_ARRAY = new Uint8Array(data);
+    const STRING_CHAR = TYPED_ARRAY.reduce((data, byte) => {
+      return data + String.fromCharCode(byte)
+    }, '');
+    let base64String = btoa(STRING_CHAR);
+    this.imageurl = this.domSanitizer.bypassSecurityTrustUrl('data:image/png;base64,' + base64String)
+
+  }
+}
