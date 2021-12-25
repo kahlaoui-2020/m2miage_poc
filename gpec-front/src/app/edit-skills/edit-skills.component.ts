f389beb3616370645d307a4de8ed1cf42f5167b5
kahlaoui-2020 <kahlaoug@gmail.com>
2021-12-25 22:15:33 +0100

    .

--- gpec-front/src/app/edit-skills/edit-skills.component.ts
+++ gpec-front/src/app/edit-skills/edit-skills.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from "../services/user.service";
 
 @Component({
   selector: 'app-edit-skills',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditSkillsComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {}
 
 }
