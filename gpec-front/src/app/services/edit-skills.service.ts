197590f8545a5e05de6dd130ef36b654424b9396
kahlaoui-2020 <kahlaoug@gmail.com>
2021-12-25 22:17:07 +0100

    update service 'get (categories, skills, levels)'

--- gpec-front/src/app/services/edit-skills.service.ts
+++ gpec-front/src/app/services/edit-skills.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+
+const optionHeader = {
+  headers: new HttpHeaders({
+    'Access-Control-Allow-Origin':'*',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  })
+}
+
+
+const  categories_url = '/api/edit-skills/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EditSkillsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  get Categories() {
+    return this.http.get<any>(environment.apiURL + categories_url + 'categories');
+  }
+
+  getSkills(id_categorie: number) {
+    let params = new HttpParams().set('id_categorie', id_categorie);
+    return this.http.get<any>(environment.apiURL + categories_url + 'skills', {params: params});
+  }
+
+
+  get Levels() {
+    return this.http.get<any>(environment.apiURL + categories_url + 'levels');
+
+  }
 }
+
