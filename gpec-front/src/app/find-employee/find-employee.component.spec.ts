90f46947052f60459a2fb6a8d772602e1ab25301
kahlaoui-2020 <kahlaoug@gmail.com>
2021-12-31 21:43:56 +0100

    component find employee

---
+++ gpec-front/src/app/find-employee/find-employee.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FindEmployeeComponent } from './find-employee.component';
+
+describe('FindEmployeeComponent', () => {
+  let component: FindEmployeeComponent;
+  let fixture: ComponentFixture<FindEmployeeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FindEmployeeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FindEmployeeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ gpec-front/src/app/find-employee/find-employee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-find-employee',
+  templateUrl: './find-employee.component.html',
+  styleUrls: ['./find-employee.component.css']
+})
+export class FindEmployeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
