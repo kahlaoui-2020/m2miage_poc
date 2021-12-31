c1f897d6b0842d4555deefdd4eeb3f0b496b019c
kahlaoui-2020 <kahlaoug@gmail.com>
2021-12-31 18:52:26 +0100

    add radar graph

--- gpec-front/src/app/profile-resume/profile-resume.component.ts
+++ gpec-front/src/app/profile-resume/profile-resume.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ChartConfiguration, ChartData, ChartType } from "chart.js";
+import {Chart} from "node_modules/chart.js";
+import {UserService} from "../services/user.service";
+
 @Component({
   selector: 'app-profile-resume',
   templateUrl: './profile-resume.component.html',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileResumeComponent implements OnInit {
 
-  constructor() { }
+  radarChartOptions = {
+    responsive: true,
+  };
+
+  radarChartLabel: string[] = ['1', '2', '3', '4', '5'];
+  radarChartData: ChartData<'radar'> = {
+    labels: this.radarChartLabel,
+    datasets: [{
+      label: '',
+      data: [100, 30, 50, 15, 92],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+      ]
+    }]
+  }
+  radarChartType: ChartConfiguration<ChartType>['type'] = 'radar';
+
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.userService.userGraph.subscribe((result: any) => {
+      console.log(result)
+    })
+
   }
 
 }
